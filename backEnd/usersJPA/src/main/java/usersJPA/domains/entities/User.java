222a0177e9df6cdf6314b807b053f653285570d0
Peter_OC <pedro.olmos-carrilero@capgemini.com>
2022-03-25 15:13:13 +0100

    Servicio de Seguridad en desarrollo

---
+++ backEnd/usersJPA/src/main/java/usersJPA/domains/entities/User.java
@@ -0,0 +1,95 @@
+package usersJPA.domains.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the users database table.
+ * 
+ */
+@Entity
+@Table(name="users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_user")
+	private int idUser;
+
+	private String address;
+
+	private String email;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	private String function;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	private String password;
+
+	public User() {
+	}
+
+	public int getIdUser() {
+		return this.idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFunction() {
+		return this.function;
+	}
+
+	public void setFunction(String function) {
+		this.function = function;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
