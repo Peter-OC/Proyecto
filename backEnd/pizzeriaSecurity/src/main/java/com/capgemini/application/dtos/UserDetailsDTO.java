8721586e62d3c6da2c3c72c4dfc884815975bc47
Peter_OC <pedro.olmos-carrilero@capgemini.com>
2022-03-26 19:58:30 +0100

    Añadidos DTO

---
+++ backEnd/pizzeriaSecurity/src/main/java/com/capgemini/application/dtos/UserDetailsDTO.java
@@ -0,0 +1,43 @@
+package com.capgemini.application.dtos;
+
+
+import com.capgemini.domains.entities.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
+public class UserDetailsDTO {
+	
+	@JsonProperty("username")
+	private String username;
+	
+	@JsonProperty("nombre")
+	private String first_name;
+
+	@JsonProperty("apellido")
+	private String last_name;
+	
+	@JsonProperty("direcion")
+	private String address;
+
+	@JsonProperty("contraseña")
+	private String password;
+	
+	@JsonProperty("rol")
+	private String function;
+	
+	public static UserDetailsDTO from(User source) {
+		return new UserDetailsDTO(
+				source.getUsername(),
+				source.getFirstName(),
+				source.getLastName(),
+				source.getAddress(),
+				source.getPassword(),
+				source.getFunction()
+				);
+	}
+
+}
