51f4fee55f25ccab8d5445267b310ecffeb5780b
Peter_OC <pedro.olmos-carrilero@capgemini.com>
2022-04-01 12:54:47 +0200

    estilo menubar

--- frontEnd/Pizzeria/src/app/app.component.ts
+++ frontEnd/Pizzeria/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component} from '@angular/core';
 import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Pizzeria';
