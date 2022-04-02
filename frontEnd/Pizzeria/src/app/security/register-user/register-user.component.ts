d3ddbe6e43878dcd05ec10905966f9ea4819a336
JMManresa <jose-manuel.manresa-guirao@capgemini.com>
2022-04-02 12:09:10 +0200

    campos de registro modificados para que concuerden con el backend

--- frontEnd/Pizzeria/src/app/security/register-user/register-user.component.ts
+++ frontEnd/Pizzeria/src/app/security/register-user/register-user.component.ts
@@ -30,6 +30,7 @@ export class RegisterUserComponent implements OnInit {
     this.miForm = new FormGroup({
       username: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20), Validators.email]),
       nombre: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
       direccion: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
       contraseña: new FormGroup({
         contraseñaValue: new FormControl('', [Validators.required, Validators.minLength(2)]),
@@ -129,8 +130,9 @@ export class RegisterUserComponent implements OnInit {
       username: data.username,
       contraseña: data.contraseña.contraseñaValue,
       nombre: data.nombre,
+      apellido: data.apellido,
       direccion: data.direccion,
-      roles: data.roles
+      //roles: data.roles
     } as User);
     this.dao.add(this.model).subscribe(
       rslt => {
