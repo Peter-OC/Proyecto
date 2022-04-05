e09e0c0503a4113633a6413dc40c9c1c7e1124b6
vero-amoros <veronica.garcia-amoros@capgemini.com>
2022-04-05 12:29:21 +0200

    Botones página principal retocados

--- frontEnd/Pizzeria/src/app/home/home.component.ts
+++ frontEnd/Pizzeria/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotificationService } from '../common-services';
+import { LoginService } from '../security';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    txtButon = 'Log In';
+    txtUsuario = '';
+    txtPassword = '';
 
-  ngOnInit(): void {
+    constructor(public loginSrv: LoginService, private notify: NotificationService, private router: Router) { }
+
+    ngOnInit() {
+      this.cambiaTexto();
+    }
+
+    logInOut() {
+      if (this.loginSrv.isAutenticated) {
+        this.loginSrv.logout();
+        this.cambiaTexto();
+      } else {
+        this.loginSrv.login(this.txtUsuario, this.txtPassword).subscribe({
+          next: data => {
+            if (data) {
+              this.cambiaTexto();
+              this.setUser(this.txtUsuario);
+            } else {
+              this.notify.add('Usuario o contraseña invalida.');
+            }
+          },
+          error: err => { this.notify.add(err.message); }
+        });
+      }
+    }
+
+    registrar() {
+      this.router.navigateByUrl('/registro');
+    }
+
+    private cambiaTexto() {
+      this.txtButon = this.loginSrv.isAutenticated ? 'Log Out' : 'Log In';
+    }
+
+    setUser(user: string) {
+      localStorage.setItem("datos", JSON.stringify(user));
+    }
   }
 
-}
+
 
